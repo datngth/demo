54836c3cc3140e93571d2d5060721c412c9594e6
datngth <131776410+datngth@users.noreply.github.com>
2023-04-25 19:41:56 +0700

    Add files via upload

---
+++ wipe.py
@@ -0,0 +1,17 @@
+import cv2
+
+pikachu = cv2.imread("data\pikachu.png", cv2.IMREAD_COLOR)
+conan = cv2.imread("data\Edogawa-Conan-.jpg", cv2.IMREAD_COLOR)
+
+pikachu = cv2.resize(pikachu, (conan.shape[1], conan.shape[0]))
+
+H = pikachu.shape[0]
+W = pikachu.shape[1]
+for d in range (0,W+1):
+    view = pikachu.copy()
+    view[0:H, 0:W-d] = pikachu[0:H, d:W]
+    view[0:H, W-d:W] = conan[0:H, 0:d]
+    cv2.imshow("View", view)
+    cv2.waitKey(1)
+
+
