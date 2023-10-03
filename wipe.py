4f3977de4a4d5dcd149ef63dadf75ab7af7b35b5
datngth <ngthdat992003@gmail.com>
2023-10-03 22:15:16 +0700

    #dsd

--- wipe.py
+++ wipe.py
@@ -7,11 +7,10 @@ pikachu = cv2.resize(pikachu, (conan.shape[1], conan.shape[0]))
 
 H = pikachu.shape[0]
 W = pikachu.shape[1]
-for d in range (0,W+1):
+for d in range(0, W+1):
     view = pikachu.copy()
     view[0:H, 0:W-d] = pikachu[0:H, d:W]
     view[0:H, W-d:W] = conan[0:H, 0:d]
     cv2.imshow("View", view)
     cv2.waitKey(1)
-print("hello)
-
+print("hello")
